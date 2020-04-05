2254efdb81acc685f7f2a92ff68ff6defda4f70b
zhs007 <sssxueren@gmail.com>
2020-04-05 18:34:31 +0800

    - Added Train2Server

---
+++ include/tradingcore2/server/train2.h
@@ -0,0 +1,13 @@
+#ifndef __TRADINGCORE2_SERVER_TRAIN2_H__
+#define __TRADINGCORE2_SERVER_TRAIN2_H__
+
+#include <tradingcore2/basedef.h>
+#include <tradingcore2/server.h>
+
+CR2BEGIN
+
+Server* newTrainServer2(const char* strBindAddr);
+
+CR2END
+
+#endif  // __TRADINGCORE2_SERVER_TRAIN2_H__
---
+++ src/tradingcore2/server/train2.cpp
@@ -0,0 +1,67 @@
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include <tradingcore2/server/train.h>
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "../proto/tradingcore2.grpc.pb.h"
+
+CR2BEGIN
+
+// using grpc::Server;
+// using grpc::ServerBuilder;
+// using grpc::ServerContext;
+// using grpc::Status;
+// using helloworld::HelloRequest;
+// using helloworld::HelloReply;
+// using helloworld::Greeter;
+
+// Logic and data behind the server's behavior.
+class TrainService2Impl final
+    : public tradingcore2pb::TradingCore2Service::Service {
+  virtual ::grpc::Status train(::grpc::ServerContext* context,
+                               const ::tradingcore2pb::TrainData* request,
+                               ::tradingcore2pb::TrainResult* response) {
+    return grpc::Status::OK;
+  }
+};
+
+class TrainServer2 final : public Server {
+ public:
+  TrainServer2(const char* strBindAddr) : m_bindAddr(strBindAddr) {}
+  virtual ~TrainServer2() {}
+
+ public:
+  virtual void run() {
+    std::string server_address(m_bindAddr.c_str());
+    TrainService2Impl service;
+
+    grpc::EnableDefaultHealthCheckService(true);
+    grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+    grpc::ServerBuilder builder;
+    // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    // Register "service" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *synchronous* service.
+    builder.RegisterService(&service);
+    // Finally assemble the server.
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Wait for the server to shutdown. Note that some other thread must be
+    // responsible for shutting down the server for this call to ever return.
+    server->Wait();
+  }
+
+ private:
+  std::string m_bindAddr;
+};
+
+Server* newTrainServer(const char* strBindAddr) {
+  return new TrainServer2(strBindAddr);
+}
+
+CR2END
