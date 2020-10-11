4c4ee7581a8cf629c32221d66605f0364eea970b
zhs007 <sssxueren@gmail.com>
2020-10-11 17:21:39 +0800

    - added calcfund

---
+++ src/calcfund/main.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <tradingcore2/tradingcore2.h>
+
+int main(int argc, char *argv[]) {
+  tr2::LogHelper log(argv[0]);
+
+  printf("trdb2client starting...\n");
+  printf("version is %s\n", tr2::getVersion());
+
+  if (argc != 2) {
+    printf("please type trdb2client cfgfile.");
+
+    return -1;
+  }
+
+  tr2::Config cfg;
+  tr2::loadConfig(cfg, argv[1]);
+
+  tradingdb2pb::Candles candles;
+  auto ret =
+      tr2::getCandles(candles, cfg.trdb2Serv.c_str(), cfg.trdb2Token.c_str(),
+                      "cnfunds", "000001", NULL, 0, 0);
+
+  printf("getCandles %s\n", ret ? "ok" : "fail");
+  printf("candles %d\n", candles.candles_size());
+
+  return 0;
+}
