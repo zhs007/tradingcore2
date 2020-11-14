de6c33f27b08485e808e8a39f817199cc810e88e
zhs007 <sssxueren@gmail.com>
2020-11-14 19:36:48 +0800

    - updated TrDB2DataMgr

--- include/tradingcore2/trdb2/datamgr.h
+++ include/tradingcore2/trdb2/datamgr.h
@@ -10,15 +10,31 @@ CR2BEGIN
 
 class TrDB2DataMgr {
  public:
-  typedef std::map<std::string, tradingpb::Candles*> _Map;
-  typedef std::pair<std::string, tradingpb::Candles*> _Pair;
+  typedef std::map<std::string, tradingpb::Candles *> _Map;
+  typedef std::pair<std::string, tradingpb::Candles *> _Pair;
 
  public:
-  TrDB2DataMgr() {}
-  ~TrDB2DataMgr() {}
+  TrDB2DataMgr(const char *host, const char *token)
+      : m_host(host), m_token(token) {}
+  ~TrDB2DataMgr() { this->release(); }
+
+ public:
+  void release();
+
+ public:
+  // addData - add data
+  bool addData(const char *market, const char *symbol,
+               std::vector<const char *> *pTags, int64_t tsStart,
+               int64_t tsEnd);
+
+  const tradingpb::Candles *getData(const char *market,
+                                    const char *symbol) const;
 
  private:
   _Map m_map;
+
+  std::string m_host;
+  std::string m_token;
 };
 
 CR2END
--- src/tradingcore2/trdb2/datamgr.cpp
+++ src/tradingcore2/trdb2/datamgr.cpp
@@ -4,6 +4,7 @@
 #include <tradingcore2/exchange.h>
 #include <tradingcore2/protos/tradingdb2.grpc.pb.h>
 #include <tradingcore2/train.h>
+#include <tradingcore2/trdb2/client.h>
 #include <tradingcore2/trdb2/datamgr.h>
 #include <tradingcore2/utils.h>
 
@@ -14,6 +15,49 @@
 
 CR2BEGIN
 
+void TrDB2DataMgr::release() {
+  for (auto it = this->m_map.begin(); it != this->m_map.end(); ++it) {
+    delete it->second;
+  }
 
+  this->m_map.clear();
+}
+
+// addData - add data
+bool TrDB2DataMgr::addData(const char *market, const char *symbol,
+                           std::vector<const char *> *pTags, int64_t tsStart,
+                           int64_t tsEnd) {
+  auto pCandles = new tradingpb::Candles();
+
+  auto ret = getCandles(*pCandles, this->m_host.c_str(), this->m_token.c_str(),
+                        market, symbol, pTags, tsStart, tsEnd);
+
+  if (!ret) {
+    return false;
+  }
+
+  std::string code = market;
+  code += ".";
+  code += symbol;
+
+  _Pair p(code, pCandles);
+  auto retIns = this->m_map.insert(p);
+
+  return retIns.second;
+}
+
+const tradingpb::Candles *TrDB2DataMgr::getData(const char *market,
+                                                const char *symbol) const {
+  std::string code = market;
+  code += ".";
+  code += symbol;
+
+  auto it = this->m_map.find(code);
+  if (it != this->m_map.end()) {
+    return it->second;
+  }
+
+  return NULL;
+}
 
 CR2END
