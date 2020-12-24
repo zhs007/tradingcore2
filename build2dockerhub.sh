d3d28fa92e13ab5db1c6869fb3a309e86311a8c5
zhs007 <sssxueren@gmail.com>
2020-12-24 19:57:59 +0800

    upd

---
+++ build2dockerhub.sh
@@ -0,0 +1,7 @@
+rm -rf ./dockerhub/trnode2
+mkdir ./dockerhub/trnode2
+docker build -f ./Dockerfile.builddockerhub -t trnode2 .
+docker run -d --name trnode2 trnode2 sh -c "while true; do sleep 1; done"
+docker cp trnode2:/app/tr2/build/src/server/tc2serv ./dockerhub/trnode2/
+docker stop trnode2
+docker rm trnode2
