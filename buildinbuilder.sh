9578100d4955e7a0c8623fd82b76ae99d772bb45
zhs007 <sssxueren@gmail.com>
2020-12-29 22:40:44 +0800

    - Fixed #98

---
+++ buildinbuilder.sh
@@ -0,0 +1,25 @@
+git submodule init
+git submodule update --recursive
+git submodule update --init --recursive
+
+sh buildgoogletest.sh
+
+sh buildgsl.sh
+
+sh buildglog.sh
+
+sh buildgflags.sh
+
+sh buildzlib.sh
+
+sh fixgrpc.sh
+
+sh buildrocksdb.sh
+
+sh buildyamlcpp.sh
+
+sh buildtalib.sh
+
+cd $PWD/build
+cmake ..
+make
