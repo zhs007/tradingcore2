20e99755cf08ca37dab65cb0076e4bb33232f4d1
zhs007 <sssxueren@gmail.com>
2021-02-03 19:40:50 +0800

    - Updated client

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -991,6 +991,103 @@ void normalTAMA5_2(const tr2::Config& cfg, const char* indicator) {
   //                                       5, 5 /* off2 */, 10, 2);
 }
 
+void normalTAMA_3(const tr2::Config& cfg) {
+  tr2::NodeClient2 client(cfg.servs[0].host.c_str(),
+                          cfg.servs[0].token.c_str());
+
+  ::tradingpb::SimTradingParams params;
+
+  auto asset0 = params.add_assets();
+  asset0->set_market("jqdata");
+  asset0->set_code("000300_XSHG|1d");
+  // auto asset1 = params.add_assets();
+  // asset1->set_market("jqdata");
+  // asset1->set_code("000300_XSHG|5m");
+
+  auto strategy0 = params.add_strategies();
+  strategy0->set_name("normal");
+  auto asset10 = strategy0->mutable_asset();
+  asset10->set_market("jqdata");
+  asset10->set_code("000300_XSHG|1d");
+
+  auto buy0 = strategy0->add_buy();
+  buy0->set_name("indicatorsp");
+  buy0->add_operators("up");
+  buy0->add_strvals("ema.29");
+
+  auto buy1 = strategy0->add_buy();
+  buy1->set_name("weekday2");
+  buy1->add_vals(4);
+  buy1->add_vals(6);
+
+  auto buy2 = strategy0->add_buy();
+  buy2->set_name("weekday2");
+  buy2->add_vals(1);
+  buy2->add_vals(4);
+  buy2->set_group(1);
+
+  auto buy3 = strategy0->add_buy();
+  buy3->set_name("indicatorsp");
+  buy3->add_operators("down");
+  buy3->add_strvals("ema.29");
+  buy3->set_group(1);
+
+  auto sell0 = strategy0->add_sell();
+  sell0->set_name("weekday");
+  sell0->add_vals(3);
+
+  auto sell1 = strategy0->add_sell();
+  sell1->set_name("ctrlconditionid");
+  sell1->add_vals(1);
+  sell1->add_strvals("buy");
+
+  auto sell2 = strategy0->add_sell();
+  sell2->set_name("weekday");
+  sell2->add_vals(5);
+  sell2->set_group(1);
+
+  auto sell3 = strategy0->add_sell();
+  sell3->set_name("ctrlconditionid");
+  sell3->add_vals(2);
+  sell3->add_strvals("buy");
+  sell3->set_group(1);
+
+  auto bp = strategy0->mutable_paramsbuy();
+  bp->set_perhandmoney(1);
+  auto sp = strategy0->mutable_paramssell();
+  sp->set_pervolume(1);
+  auto ip = strategy0->mutable_paramsinit();
+  ip->set_money(10000);
+  // auto aip = strategy0->mutable_paramsaip();
+  // aip->set_money(10000);
+  // aip->set_type(tradingpb::AIPTT_WEEKDAY);
+  // aip->set_day(3);
+
+  params.set_startts(tr2::str2timestampUTC("20130501", "%Y%m%d"));
+  params.set_endts(tr2::str2timestampUTC("20200930", "%Y%m%d"));
+  // params.set_startts(0);
+  // params.set_endts(-1);
+
+  ::tradingpb::ReplyCalcPNL res;
+  auto status = client.clacPNL(params, res);
+
+  // client.waitStop();
+
+  // ::tradingpb::ReplyServerInfo res;
+  // auto status = client.getServerInfo(res);
+  LOG(INFO) << "calcPNL " << status.error_code();
+
+  if (status.ok()) {
+    tr2::logProtobuf("reply ", res);
+    // LOG(INFO) << res.DebugString();
+  }
+
+  // auto cnfund = tr2::ExchangeMgr::getSingleton()->getExchange("cnfund");
+  // tr2::startTrainSingleIndicator2ExPool(cfg, *cnfund, "110022", "rsi",
+  //                                       "../output", 10000, 5, 5 /* off0 */,
+  //                                       5, 5 /* off2 */, 10, 2);
+}
+
 void normalEMA15(const tr2::Config& cfg) {
   tr2::NodeClient2 client(cfg.servs[0].host.c_str(),
                           cfg.servs[0].token.c_str());
@@ -1097,7 +1194,8 @@ int main(int argc, char* argv[]) {
   // normalEMA15(cfg);
   // normalTAMA5_2(cfg, "ta-ema.29>day/1d/5m/53700");
   // moneyParts(cfg);
-  nextBuy(cfg);
+  // nextBuy(cfg);
+  normalTAMA_3(cfg);
 
   return 0;
 }
