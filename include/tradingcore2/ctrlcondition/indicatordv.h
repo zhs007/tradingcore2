4e9b383aba3ae94b27966f1378d8dd1595edcbe1
zhs007 <sssxueren@gmail.com>
2021-03-09 20:18:01 +0800

    - Added indicatordv

---
+++ include/tradingcore2/ctrlcondition/indicatordv.h
@@ -0,0 +1,63 @@
+#ifndef __TRADINGCORE2_CTRLCONDITION_INDICATORDV_H__
+#define __TRADINGCORE2_CTRLCONDITION_INDICATORDV_H__
+
+#include <tradingcore2/basedef.h>
+#include <tradingcore2/candle.h>
+#include <tradingcore2/config.h>
+#include <tradingcore2/ctrlcondition.h>
+#include <tradingcore2/protos/trading2.pb.h>
+
+#include <functional>
+#include <map>
+#include <string>
+
+CR2BEGIN
+
+class CCIndicatorDV final : public CtrlConditionHelper {
+ public:
+  static void regCtrlConditionHelper();
+
+ protected:
+  struct _Data {
+    int lastState;
+
+    _Data() : lastState(0) {}
+
+    void clear() {}
+  };
+
+ protected:
+  CCIndicatorDV() {}
+  virtual ~CCIndicatorDV() {}
+
+ public:
+  virtual void getIndicators(std::set<std::string>& indicators,
+                             const tradingpb::CtrlCondition& cc) override;
+
+  virtual void* newCtrlConditionData() override { return new _Data(); }
+
+  virtual void deleteCtrlConditionData(void* pData) override {
+    assert(pData != NULL);
+    auto pMyData = static_cast<_Data*>(pData);
+    delete pMyData;
+  }
+
+  virtual void clearCtrlConditionData(void* pData) override {
+    assert(pData != NULL);
+    auto pMyData = static_cast<_Data*>(pData);
+    pMyData->clear();
+  }
+
+  virtual bool isValid(const tradingpb::CtrlCondition& cc,
+                       CtrlType ct) override;
+
+  virtual bool canCtrl(const Exchange& exchange, const Wallet& wallet,
+                       const IndicatorMap& mapIndicators,
+                       const tradingpb::CtrlCondition& cc, bool issim,
+                       CtrlType ct, TimeStamp ts, int index, CandleData& cd,
+                       Assets& assets, void* pData) override;
+};
+
+CR2END
+
+#endif  // __TRADINGCORE2_CTRLCONDITION_INDICATORDV_H__
---
+++ src/tradingcore2/ctrlcondition/indicatordv.cpp
@@ -0,0 +1,81 @@
+#include <math.h>
+#include <tradingcore2/ctrlcondition/indicatordp.h>
+#include <tradingcore2/ctrlconditionmgr.h>
+#include <tradingcore2/indicatormap.h>
+
+#include <functional>
+
+CR2BEGIN
+
+// 因为是数值相关模式，和 indicatorsv 比较，唯一的区别就是2个数值指标比较，且
+// 都是 第一种 对 第二种的。
+
+void CCIndicatorDP::regCtrlConditionHelper() {
+  CtrlConditionMgr::getSingleton()->regCtrlCondition("indicatordp",
+                                                     new CCIndicatorDP());
+}
+
+void CCIndicatorDP::getIndicators(std::set<std::string>& indicators,
+                                  const tradingpb::CtrlCondition& cc) {
+  indicators.insert(cc.strvals(0));
+  indicators.insert(cc.strvals(1));
+}
+
+bool CCIndicatorDP::isValid(const tradingpb::CtrlCondition& cc, CtrlType ct) {
+  return cc.strvals_size() == 2 && cc.operators_size() == 1;
+}
+
+bool CCIndicatorDP::canCtrl(const Exchange& exchange, const Wallet& wallet,
+                            const IndicatorMap& mapIndicators,
+                            const tradingpb::CtrlCondition& cc, bool issim,
+                            CtrlType ct, TimeStamp ts, int index,
+                            CandleData& cd, Assets& assets, void* pData) {
+  auto pIndicator0 = mapIndicators.getIndicator(cc.strvals(0).c_str());
+  auto pIndicator1 = mapIndicators.getIndicator(cc.strvals(1).c_str());
+  if (pIndicator0 != NULL && pIndicator1 != NULL) {
+    assert(pData != NULL);
+    auto cv0 = pIndicator0->getSingleValue(index);
+    assert(cv0 != NULL);
+
+    auto cv1 = pIndicator1->getSingleValue(index);
+    assert(cv1 != NULL);
+
+    auto pMyData = static_cast<_Data*>(pData);
+
+    bool canctrl = false;
+
+    if (cc.operators(0) == "up") {
+      if (cv0->value > cv1->value) {
+        canctrl = true;
+      }
+    } else if (cc.operators(0) == "down") {
+      if (cv0->value < cv1->value) {
+        canctrl = true;
+      }
+    } else if (cc.operators(0) == "upcross") {
+      if (cv0->value > cv1->value && pMyData->lastState <= 0) {
+        canctrl = true;
+      }
+    } else if (cc.operators(0) == "downcross") {
+      if (cv0->value < cv1->value && pMyData->lastState >= 0) {
+        canctrl = true;
+      }
+    }
+
+    if (cv0->value > cv1->value) {
+      pMyData->lastState = 1;
+    } else if (cv0->value < cv1->value) {
+      pMyData->lastState = -1;
+    } else if (cv0->value == cv1->value) {
+      pMyData->lastState = 0;
+    }
+
+    return canctrl;
+  } else {
+    LOG(INFO) << "no indicator " << cc.strvals(0) << " || " << cc.strvals(1);
+  }
+
+  return false;
+}
+
+CR2END
