942d42258c407daba155cab97055c0c54b45da50
zhs007 <sssxueren@gmail.com>
2021-04-15 12:46:02 +0000

    Added indicatorsvarea

---
+++ include/tradingcore2/ctrlcondition/indicatorsvarea.h
@@ -0,0 +1,46 @@
+#ifndef __TRADINGCORE2_CTRLCONDITION_INDICATORSVAREA_H__
+#define __TRADINGCORE2_CTRLCONDITION_INDICATORSVAREA_H__
+
+#include <tradingcore2/basedef.h>
+#include <tradingcore2/candle.h>
+#include <tradingcore2/config.h>
+#include <tradingcore2/ctrlcondition.h>
+#include <tradingcore2/protos/trading2.pb.h>
+
+#include <functional>
+#include <map>
+#include <string>
+
+CR2BEGIN
+
+class CCIndicatorSVArea final : public CtrlConditionHelper {
+ public:
+  static void regCtrlConditionHelper();
+
+ protected:
+  CCIndicatorSVArea() {}
+  virtual ~CCIndicatorSVArea() {}
+
+ public:
+  virtual void getIndicators(std::set<std::string>& indicators,
+                             const tradingpb::CtrlCondition& cc) override;
+
+  virtual void* newCtrlConditionData() override { return NULL; }
+
+  virtual void deleteCtrlConditionData(void* pData) override {}
+
+  virtual void clearCtrlConditionData(void* pData) override {}
+
+  virtual bool isValid(const tradingpb::CtrlCondition& cc,
+                       CtrlType ct) override;
+
+  virtual bool canCtrl(const Exchange& exchange, const Wallet& wallet,
+                       const IndicatorMap& mapIndicators,
+                       const tradingpb::CtrlCondition& cc, bool issim,
+                       CtrlType ct, TimeStamp ts, int index, CandleData& cd,
+                       Assets& assets, void* pData) override;
+};
+
+CR2END
+
+#endif  // __TRADINGCORE2_CTRLCONDITION_INDICATORSVAREA_H__
